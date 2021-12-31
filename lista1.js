8a7a5d298e2391e67468c68e801eb9a084229b33
Tawara Martins <tawara.dev@gmail.com>
2021-12-30 21:39:47 -0300

    Add lista1.js

---
+++ lista1.js
@@ -0,0 +1,72 @@
+// Lista 1 - Expressões, entrada e saída de dados
+// Leia dois números inteiros e escreva o resultado de sua soma.
+let a = Number(prompt("Digite um numero"));
+let b = Number(prompt("Digite um outro numero"));
+console.log(a + b);
+
+// Leia dois números inteiros e escreva o resultado de sua subtração.
+let a = parseInt(prompt("Digite um numero."));
+let b = parseInt(prompt("Digite outro numero."));
+console.log(a - b);
+
+// Leia dois números inteiros e escreva o resultado de sua multiplicação.
+let a = parseInt(prompt("Digite um numero."));
+let b = parseInt(prompt("Digite um outro numero."))
+console.log(a * b);
+
+// Leia dois números inteiros (a e b) e escreva o resultado da divisão a / b.
+let a = parseInt(prompt("Digite um numero."));
+let b = parseInt(prompt("Digite um outro numero"));
+console.log(a / b);
+
+// Leia dois números inteiros (a e b) e escreva o resultado do resto da divisão a % b.
+let a = parseInt(prompt("Digite um numero"));
+let b = parseInt(prompt("Digite um outro numero"));
+console.log(a % b);
+
+// Leia dois números reais e escreva o resultado de sua soma.
+let a = parseFloat(prompt("Digite um numero"));
+let b = parseFloat(prompt("Digite outro numero"));
+console.log(a + b);
+
+// Leia o nome de uma pessoa e escreva uma frase de saudação, como: Olá, Fulano.
+let name = prompt("Digite aqui o seu nome.");
+alert(`Olá, ${name}`);
+
+// A média aritmética de dois ou mais termos é o quociente do resultado da divisão da soma dos números dados pela quantidade de números somados. Elabore um algoritmo que leia as 4 notas de um aluno e escreva sua média aritmética.
+let n1 = Number(prompt("Digite aqui a nota 1."));
+let n2 = Number(prompt("Digite aqui a nota 2"));
+let n3 = Number(prompt("Digite aqui a nota 3"));
+let n4 = Number(prompt("Digite aqui a nota 4"));
+
+console.log(n1 + n2 + n3 + n4 / 4);
+
+// A autonomia de um veículo é baseada em quantos quilômetros ele consegue percorrer a cada litro de combustível consumido. Por exemplo, é comum afirmar que tal carro é capaz de fazer 15 quilômetros por litro de combustível. Com base nisso, elabore um algoritmo que leia uma distância percorrida em quilômetros e uma quantidade de litros de combustível consumidos, e informe quantos quilômetros este veículo pode percorrer com cada litro.
+
+let PL = Number(prompt("Digite aqui o Km do carro."));
+let KL = Number(prompt("Digite aqui o litro."));
+let KT = Number(prompt("Digite aqui o preco."));
+let resultado = KT * (PL / KL);
+console.log(`O veiculo pode percorrer ${resultado} por cada litro`);
+
+// A velocidade média de um veículo pode ser calculada se tivermos os seguintes valores: a distância percorrida e o tempo gasto para percorrer esta distância. Com base nisso, elabore um algoritmo que leia uma distância percorria em quilômetros e o tempo gasto em horas, e informe a velocidade média desse veículo.
+
+let position_final = Number(prompt("Digite o final da partida aqui."));
+let position_inicial = Number(prompt("Digite o inicial da partida aqui."));
+let time_final = Number(prompt("Digite o final do tempo aqui."));
+let time_inicial = Number(prompt("Digite o inicial do tempo aqui."));
+let pf_pi = position_final - position_inicial;
+let tf_ti = time_final - time_inicial;
+console.log(`A velocidade média do veiculo e de : `, pf_pi / tf_ti);
+
+
+// Alguns países de língua inglesa, como os EUA, utilizam da unidade de medida grau Fahrenheit como escala de temperatura. Para converter uma temperatura de Celsius para Fahrenheit, podemos aplicar a fórmula F=(9*C+160)/5 ou a fórmula F=C*1.8+32, onde F é a temperatura em Fahrenheit e C a temperatura em Celsius. Elabore um algoritmo que leia uma temperatura em graus Celsius e a escreva convertida em graus Fahrenheit.
+
+
+
+// Para auxiliar uma loja que deseja parcelar o valor das compras de seus clientes, elabore um algoritmo que leia o valor de uma compra e o número de prestações desejadas, e escreva o valor das prestações.
+// No comércio, os preços dos produtos geralmente são definidos com base em uma margem percentual de lucro. Considere um produto adquirido por uma loja pelo valor de R$ 100,00. Se esta loja aplicar a este produto um percentual de 15% de lucro, o produto será revendido pelo valor de R$ 115,00. Elabore um algoritmo que leia o valor de compra de um produto e o percentual de lucro desejado, e escreva o valor de revenda deste produto.
+// Você foi encarregado de realizar uma pesquisa sobre Inclusão Digital. A sua pesquisa deverá apresentar o percentual de alunos da sua escola que possuem acesso à internet. Para isso, elabore um algoritmo que leia o número total de alunos da sua escola e o número de alunos que possuem acesso à internet, por fim, com base nestes dados, escreva o percentual de alunos com acesso à internet. Ex.: Em uma escola com 200 alunos, apenas 50 alunos possuem acesso à internet, o que equivale a 25% destes 200 alunos.
+//Em uma pequena loja de eletrônica são vendidos resistores, diodos e transistores. Seus preços são R$ 0.70, R$ 0.80 e R$ 0.95, respectivamente. Têm-se os preços unitários de cada tipo de peça e sabe-se que sobre estes preços incidem descontos de 5% para resistores, 6% para diodos e 9% para transistores. Elabore um programa que leia o nome do cliente e as quantidades de cada item comprado; e escreva o valor total de desconto e o valor líquido a ser pago pela compra.
+
+//Lista 2 - Estruturas de decisão: Se/Senão
